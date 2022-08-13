78c0a3b8160368ec4ff5dad1a955ec48751ceffe
skshadab1234 <ks615044@gmail.com>
2022-08-13 21:50:22 +0530

    Question Testpage Functionalitiy Added

--- client/next.config.js
+++ client/next.config.js
@@ -1,40 +1,3 @@
-// module.exports = () => {
-//   const rewrites = () => {
-//     return [
-//       {
-//         source: "/login",
-//         destination: "http://localhost:3001/login",
-//       },
-//       {
-//         source: "/profile",
-//         destination: "http://localhost:3001/profile",
-//       },
-//       {
-//         source: "/getdata",
-//         destination: "http://localhost:3001/getdata",
-//       },
-//       {
-//         source: "/logout",
-//         destination: "http://localhost:3001/logout",
-//       },
-//       {
-//         source: "/register",
-//         destination: "http://localhost:3001/register",
-//       },
-//       {
-//         source: "/uploadTest",
-//         destination: "http://localhost:3001/uploadTest",
-//       },
-//     ];
-//   };
-//   return {
-//     rewrites,
-//   };
-// };
-
-
-// Server
-
 module.exports = () => {
   const rewrites = () => {
     return [
@@ -62,7 +25,6 @@ module.exports = () => {
         source: "/uploadTest",
         destination: "https://ciiyc-2022.herokuapp.com/uploadTest",
       },
-      
     ];
   };
   return {
